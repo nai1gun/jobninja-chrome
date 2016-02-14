4355d29b01c938b8a3ec318523fee728056a4cdc
Nailgun <djkp9ik@gmail.com>
2016-02-14 21:15:47 +0100

    [+] login and save to local storage

--- app/scripts.babel/auth.js
+++ app/scripts.babel/auth.js
@@ -2,39 +2,56 @@
 'use strict';
 
 angular.module('jobninja')
-    .factory('Auth', ['config', '$http', function loginService(config, $http) {
+    .factory('Auth', ['config', '$http', '$q', function loginService(config, $http, $q) {
         return {
             login: function(credentials) {
-                var data = 'username=' + credentials.username + '&password='
-                    + credentials.password + '&grant_type=password&scope=read%20write&' +
-                    'client_secret=mySecretOAuthSecret&client_id=jhtestapp';
-                return $http.post(config.apiBaseUrl + 'oauth/token', data, {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Accept': 'application/json',
-                        'Authorization': 'Basic ' + btoa('jhtestapp' + ':' + 'mySecretOAuthSecret')
-                    }
-                }).success(function (response) {
-                    var expiredAt = new Date();
-                    expiredAt.setSeconds(expiredAt.getSeconds() + response.expires_in);
-                    response.expiredAt = expiredAt.getTime();
-                    //localStorageService.set('token', response);
-                    console.log('Authenticated with token: ' + angular.toJson(response));
-                    return response;
+                return $q(function(resolve, reject) {                  
+                    var data = 'username=' + credentials.username + '&password='
+                        + credentials.password + '&grant_type=password&scope=read%20write&' +
+                        'client_secret=mySecretOAuthSecret&client_id=jhtestapp';
+                    $http.post(config.apiBaseUrl + 'oauth/token', data, {
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Accept': 'application/json',
+                            'Authorization': 'Basic ' + btoa('jhtestapp' + ':' + 'mySecretOAuthSecret')
+                        }
+                    }).then(function(response) {
+                        var token = response.data;
+                        var expiredAt = new Date();
+                        expiredAt.setSeconds(expiredAt.getSeconds() + token.expires_in);
+                        token.expiredAt = expiredAt.getTime();
+                        chrome.storage.local.set({'token': token}, function() {
+                            resolve(token);
+                        });
+                    }, function(errorResponse) {                    
+                        reject(errorResponse.data);
+                    });
                 });
             },
             logout: function() {
-                // logout from the server
-                $http.post(config.apiBaseUrl + 'api/logout').then(function() {
-                    //localStorageService.clearAll();
+                return $q(function(resolve, reject) {
+                    $http.post(config.apiBaseUrl + 'api/logout').then(function() {
+                        chrome.storage.local.remove('token', function() {
+                            resolve();
+                        });
+                    }, function(errorResponse) {
+                        reject(errorResponse.data);
+                    });   
                 });
             },
             getToken: function () {
-                //return localStorageService.get('token');
+                return $q(function(resolve) {
+                    chrome.storage.local.get('token', function(token) {
+                        resolve(token);
+                    });
+                });
             },
             hasValidToken: function () {
-                var token = this.getToken();
-                return token && token.expiredAt && token.expiredAt > new Date().getTime();
+                return $q(function(resolve) {
+                    this.getToken().then(function(token) {
+                        resolve(token && token.expiredAt && token.expiredAt > new Date().getTime());
+                    });
+                });
             }
         };
     }]);
--- app/scripts.babel/login.js
+++ app/scripts.babel/login.js
@@ -16,8 +16,21 @@ angular.module('jobninja')
         $scope.password = '';
 
         $scope.doLogin = function() {
-            Auth.login({username: $scope.login, password: $scope.password});
-            console.log('Login success with: ' + $scope.login + ': ' + $scope.password);
+            Auth.login({username: $scope.login, password: $scope.password}).then(function(token) {
+                console.log('Login success with: ' + $scope.login + ': ' + $scope.password);
+                console.log('Token: ' + angular.toJson(token));
+            },
+            function(err) {
+                if (err && err.error === 'invalid_grant') {
+                    console.log('Wrong credentials');
+                } else {
+                    var errMsg = 'Unexpected error';
+                    if (err && err.err.error_description) {
+                        errMsg += ' ' + err.error_description;
+                    }
+                    console.log(errMsg);
+                }
+            });
         };
 
     });
