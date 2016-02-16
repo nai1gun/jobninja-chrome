4fa639a87c63a444e9a5539402ce6556f7c2c4c5
Nailgun <djkp9ik@gmail.com>
2016-02-16 23:38:20 +0100

    [+] grab position info from page and send to extension controller

--- app/scripts.babel/grab-content.js
+++ app/scripts.babel/grab-content.js
@@ -1,11 +1,50 @@
-/* global $ */
+/* global $, chrome */
 'use strict';
-$(function() {
-    var jobPosting = $('[itemtype="http://schema.org/JobPosting"]')[0];
-    console.log(jobPosting);
-    if (jobPosting) {
-        console.log('Found job posting on the page :)');
-    } else {
-        console.log('Job posting content not found :(');
-    }
-});
+
+var JOB_POSTING_ELEMENT = '[itemtype="http://schema.org/JobPosting"]';
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log('Got message');
+        if (request.method === 'hasPosition') {
+            sendResponse(hasPosition());
+        } else if (request.method === 'grabPosition') {
+            sendResponse(grabPosition());
+        } else {
+            console.log('Unrecognized message');
+        }
+    });
+
+function hasPosition() {
+    return new Boolean($(JOB_POSTING_ELEMENT)[0]);
+}
+
+function getJobPosting() {
+    return $(JOB_POSTING_ELEMENT);
+}
+
+function getName() {
+    return getJobPosting().find('[itemprop="title"]').text();
+}
+
+function getCompany() {
+    return $('[itemtype="http://schema.org/Organization"]').find('[itemprop="name"]').text();
+}
+
+function getLocation() {
+    return $('[itemtype="http://schema.org/PostalAddress"]').text();
+}
+
+function getHref() {
+    return window.location.href;
+}
+
+function grabPosition() {
+    var position = {
+        name: getName(),
+        company: getCompany(),
+        location: getLocation(),
+        link: getHref()
+    };
+    return position;
+}
--- app/scripts.babel/grab.js
+++ app/scripts.babel/grab.js
@@ -9,8 +9,31 @@ angular.module('jobninja')
                 controller: 'GrabController'
             });
     })
-    .controller('GrabController', () => {
+    .controller('GrabController', ($scope) => {
 
+        $scope.hasPosition = null;
 
+        $scope.grabPosition = function() {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {method: 'grabPosition'}, function(response) {
+                    console.log('got position: ' + angular.toJson(response));
+                });
+            });
+        };
+
+        function loadHasPosition() {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {method: 'hasPosition'}, function(response) {
+                    $scope.$apply(function () {
+                        $scope.hasPosition = response;
+                    });
+                });
+            });
+        }
+
+        loadHasPosition();
+
+    })
+    .service('GrabService', () => {
 
     });
